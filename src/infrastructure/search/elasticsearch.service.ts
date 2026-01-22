eb066a6c4609325f1e4d59f0cd3e00be61743d34
kardasch404 <zz2406143@gmail.com>
2026-01-22 11:23:02 +0100

    feat(catalog): configure elasticsearch client

--- src/infrastructure/search/elasticsearch.service.ts
+++ src/infrastructure/search/elasticsearch.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Client } from '@elastic/elasticsearch';
+import { ELASTICSEARCH_CONFIG } from './elasticsearch.config';
 
 @Injectable()
 export class ElasticsearchService implements OnModuleInit {
@@ -12,6 +13,20 @@ export class ElasticsearchService implements OnModuleInit {
     this.client = new Client({
       node: this.configService.get('ELASTICSEARCH_NODE', 'http://localhost:9200'),
     });
+    await this.createIndex();
+  }
+
+  async createIndex() {
+    const exists = await this.client.indices.exists({ index: ELASTICSEARCH_CONFIG.INDEX });
+    if (!exists) {
+      await this.client.indices.create({
+        index: ELASTICSEARCH_CONFIG.INDEX,
+        body: {
+          settings: ELASTICSEARCH_CONFIG.SETTINGS,
+          mappings: ELASTICSEARCH_CONFIG.MAPPINGS,
+        },
+      });
+    }
   }
 
   getClient(): Client {
